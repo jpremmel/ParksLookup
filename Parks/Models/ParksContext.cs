b5bcd3b93d26bd5d419b0aeafabefd7bac94aee5
Jacqueline Remmel <jpremmel@gmail.com>
2019-11-01 09:46:38 -0700

    Add seed data

--- Parks/Models/ParksContext.cs
+++ Parks/Models/ParksContext.cs
@@ -10,5 +10,14 @@ namespace Parks.Models
         }
 
         public DbSet<Park> Parks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Park>()
+                .HasData(
+                    new Park { ParkId = 1, Name = "Death Valley National Park", Location = "Eastern California; Nevada", SquareMileage = 5262, Terrain = "Harsh desert, salt-flats, sand dunes, badlands, valleys, canyons, mountains", Description = "The hottest, driest and lowest of all the national parks in the United States." },
+                    new Park { ParkId = 2, Name = "Wrangell-St. Elias National Park & Preserve", Location = "Eastern region of South-central Alaska", SquareMileage = 20587, Terrain = "Mountains, glaciers, tundra, forest, tidewaters, alpine", Description = "The largest national park in the United States. This park includes a large portion of the Saint Elias Mountains, which include most of the highest peaks in the United States and Canada." }
+                );
+        }
     }
 }
