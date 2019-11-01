49806dafa0144dc38e9e7488a00e0fc83787a768
Jacqueline Remmel <jpremmel@gmail.com>
2019-11-01 08:24:44 -0700

    Add webapi project setup

---
+++ Parks/Models/Park.cs
@@ -0,0 +1,10 @@
+namespace Parks.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string  Location { get; set; }
+        
+    }
+}
